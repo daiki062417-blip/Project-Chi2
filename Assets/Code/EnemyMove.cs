17f577217395c386de5fc8df1fcc0d419150ab1d
Ken-S039 <s0624039@iwate-u.ac.jp>
2026-05-13 12:09:46 +0900

    kengo 5 13 12:09

--- Assets/Code/EnemyMove.cs
+++ Assets/Code/EnemyMove.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class EnemyMove : Character
 {
+    //敵キャラの座標
     public Transform ThisTransform;
-    private bool movable = false;
+
+    //敵キャラの認知範囲にいるかどうか
+    private bool in_area = false;
+
     //操作キャラは変動するのでMainで用いている操作キャラを示す変数を利用した。もっといい方法はありますか
-    GameObject Main;
+    public PlayerManager Player;
 
     
     void Start()
@@ -16,23 +20,38 @@ public class EnemyMove : Character
         //自身の座標情報
         ThisTransform = GetComponent<Transform>();
 
-        Main = GameObject.Find("Main");
+        //操作キャラの情報を得るため
+        //Main = GameObject.Find("Main").GetComponent<ChangeMyChara>();
+
 
     }
 
     void Update()
     {
-        if (movable = Player_In_Area())
+        if (in_area = Player_In_Area(ThisTransform, Player.Playable().transform))
         {
             Debug.Log("敵の範囲内に入りました");
         }
+
+        //
     }
 
-    bool Player_In_Area()
-    {        
+    bool Player_In_Area(Transform enemy, Transform player)
+    {
         //範囲は円形範囲で、半径はとりあえず仮で決めた。
-        if(Vector3.Distance(ThisTransform.position, Main.GetComponent<ChangeMyChara>().Playable().transform.position) < 5f)
+        float dis = Vector3.Distance(enemy.position, player.position);
+
+        //敵が攻撃をするのは範囲内
+        if(dis < 5f)
         {
+            //敵が移動するのは範囲内であるときだが、一定まで近づけば移動しなくていい
+            if (dis > 1f)
+            {
+                //とりあえずスピードは１ｆにしている操作キャラに向かうベクトルを元に座標を代入
+                //現時点では直線的、単調に向かっていくだけなのでご了承ください
+                enemy.position =
+                    Vector3.MoveTowards(enemy.position, player.position, 1f * Time.deltaTime);
+            }
             return true;
         }
 
