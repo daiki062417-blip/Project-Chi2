11c4496c6a676e152e322f0b8d46be4e83719aa1
daiki062417 <daiki062417@gmail.com>
2026-06-04 10:58:35 +0900

    Update 仕様書 & Create Scenario

---
+++ Assets/Code/SenarioManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// シナリオのフラグ監視、ロード、終了後の処理を行うクラス。
+/// </summary>
+public class SenarioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool  ObserveFlag()
+}
